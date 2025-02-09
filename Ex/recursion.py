26a8c599665231c1078b3fb5fd54f23d94e1ead0
winxtxr <winnyjt2006@gmail.com>
2025-02-10 00:16:38 +0700

    Update

--- Ex/recursion.py
+++ Ex/recursion.py
@@ -1,9 +1,9 @@
 def recursive_sum(lst):
     if len(lst) == 0:
-        return 0  # Base case: If the list is empty, return 0
-    return lst[0] + recursive_sum(lst[1:])  # Recursive step
+        return 0  
+    return lst[0] + recursive_sum(lst[1:])  
 
 if __name__ == '__main__':
     a = [10, 5, 2, 8, 7]
     x = recursive_sum(a)
-    print(x)  # Output: 32
+    print(x)  
